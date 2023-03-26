30e0df4866ccb99c3ed38d2d98781de458fb9ad4
Anushka Verma <93106932+theanushka-v18@users.noreply.github.com>
2023-03-26 23:05:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const age = document.querySelector("#age");
+const name = document.querySelector("#name");
+const button = document.querySelector("#btn");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+	if(age.value && name.value) {
+		let promise = new Promise((resolve, reject) => {
+			setTimeout(() => {
+				if (age.value >= 18) {
+					resolve(`Welcome, ${name.value}. You can vote.`)
+				} else {
+					reject(`Oh sorry ${name.value}. You aren't old enough.`)
+				}
+			}, 4000)
+		});
+
+		promise.then((data) => {
+			alert(data);
+		}).catch((err) => {
+			alert(err);
+		})
+		
+	}
+})
